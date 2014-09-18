dc3c9c5124951d079d2ba310ef041501318f0bc3
zhongkx <zhongkx@reocar.com>
2014-09-18 23:58:36 +0800

    Add validate and default scope to sort updated_at

--- app/models/card.rb
+++ app/models/card.rb
@@ -3,5 +3,8 @@ class Card < ActiveRecord::Base
 
   validates_presence_of :question, :answer
   validates_associated :user
+  validates :question, :answer, length: { minimum: 5 }
+
+  default_scope { order 'updated_at DESC' }
 
 end
